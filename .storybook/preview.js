d21e80bd3136e73a7176a1e5927280f0c28b1c4b
Daniel Flores Medina <78251680+Dev-dfm@users.noreply.github.com>
2021-06-08 13:56:04 +0200

    Initial commit

---
+++ .storybook/preview.js
@@ -0,0 +1,14 @@
+import { addDecorator } from '@storybook/react';
+import { MemoryRouter } from 'react-router-dom';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+addDecorator((story) => <MemoryRouter>{story()}</MemoryRouter>);

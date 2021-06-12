e2583817e8c6a0a02977264dda938097ce8f3664
Dev-dfm <d.floresmedina@gmail.com>
2021-06-12 14:10:39 +0200

    ✨ Add Logo, DesginElement and background file

---
+++ src/app/components/DesignElement/DesginElementLogin.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function DesginElementLogin() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 375.001 136.588">
+      <g transform="translate(0 -675.413)">
+        <path
+          d="M1637-1038H1262v-136.587l375,79.708V-1038h0Z"
+          transform="translate(-1262 1850)"
+          fill="var(--primary-color)"
+        />
+        <path
+          d="M1637-1038H1262v-34.7l375-66.123V-1038Z"
+          transform="translate(-1262 1850)"
+          fill="var(--secondary-color)"
+        />
+      </g>
+    </svg>
+  );
+}

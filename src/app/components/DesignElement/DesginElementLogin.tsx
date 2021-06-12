11f5e66fd8d45773a576dcb0f83e8d84f4b92aea
Dev-dfm <d.floresmedina@gmail.com>
2021-06-12 14:21:49 +0200

    🏷️  Add type to DesignElement

--- src/app/components/DesignElement/DesginElementLogin.tsx
+++ src/app/components/DesignElement/DesginElementLogin.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function DesginElementLogin() {
+export default function DesginElementLogin(): JSX.Element {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 375.001 136.588">
       <g transform="translate(0 -675.413)">

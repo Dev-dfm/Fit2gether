9d825cc5eb5ab798c802b33f68f150097c3b46d9
Dev-dfm <d.floresmedina@gmail.com>
2021-06-15 10:33:32 +0200

    ♻️  Adjustecode for BackButton

--- src/app/components/BackButton/BackButton.stories.tsx
+++ src/app/components/BackButton/BackButton.stories.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import BackButton from './BackButton';
-import BackButtonIcon from '../Icons/BackButtonIcon';
 
 export default {
   title: 'Component/BackButton',
   component: BackButton,
 };
 
-export const BackButtonComponent = (): JSX.Element => (
-  <BackButton icon={<BackButtonIcon />} />
-);
+export const BackButtonComponent = (): JSX.Element => <BackButton />;
--- src/app/components/Icons/BackButtonIcon.tsx
+++ src/app/components/Icons/BackButtonIcon.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 
 export default function Backbutton(): JSX.Element {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="14.001"
-      viewBox="0 0 20 14.001"
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 14.001">
       <path
         d="M25,15H9.14l3.63-4.36a1,1,0,0,0-1.54-1.28l-5,6a1.19,1.19,0,0,0-.09.15.127.127,0,0,1-.07.13A1,1,0,0,0,6,16H6a1,1,0,0,0,.07.36.127.127,0,0,0,.07.13,1.19,1.19,0,0,0,.09.15l5,6a1,1,0,1,0,1.54-1.28L9.14,17H25a1,1,0,0,0,0-2Z"
         transform="translate(-6 -8.999)"

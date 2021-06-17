29a143afa1a21587349cb94b86494429cf4fbf1d
Dev-dfm <d.floresmedina@gmail.com>
2021-06-17 20:50:04 +0200

    ✨ Add icons for NavBar

---
+++ src/app/components/Icons/ProfileIcon.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function ProfileIcon(): JSX.Element {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" viewBox="0 0 25 25">
+      <path
+        d="M376.5,2011.5a5,5,0,1,1,5-5,5,5,0,0,1-5,5m4.7.841a7.5,7.5,0,1,0-9.4,0A12.022,12.022,0,0,0,364,2024h2.5c0-6.249,4.486-10,10-10s10,3.749,10,10H389a12.022,12.022,0,0,0-7.8-11.656"
+        transform="translate(-364 -1998.995)"
+        fill="currentColor"
+        fill-rule="evenodd"
+      />
+    </svg>
+  );
+}

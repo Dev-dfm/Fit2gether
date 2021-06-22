19e7a23a94e082f28ff694809ab5e18f9e46819d
Dev-dfm <d.floresmedina@gmail.com>
2021-06-22 13:56:31 +0200

    ✨ Implement MongoDB and sample

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,38 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
 import express from 'express';
 import path from 'path';
+import { connectDB } from './utils/database';
+import { readGroups, readGroup, saveGroup, deleteGroup } from './utils/groups';
+
+if (process.env.MONGODB_URL === undefined) {
+  throw new Error('Missing env MONGODB_URL');
+}
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
+
+app.use(express.json());
+
+app.get('/api/groups', async (_req, res) => {
+  const groups = await readGroups();
+  res.json(groups);
+});
+
+app.get('/api/groups/:groupname', async (req, res) => {
+  const groups = await readGroup(req.params.groupname);
+  res.json(groups);
+});
+
+app.post('/api/groups', async (req, res) => {
+  await saveGroup(req.body);
+  res.send('Group saved in database');
+});
+
+app.delete('/api/groups/:groupname', async (req, res) => {
+  await deleteGroup(req.params.groupname);
+  res.send('Group has been deleted');
+});
 
 // Serve storybook production bundle
 app.use('/storybook', express.static('dist/storybook'));
@@ -14,11 +40,14 @@ app.use('/storybook', express.static('dist/storybook'));
 // Serve app production bundle
 app.use(express.static('dist/app'));
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
-
 // Handle client routing, return all requests to the app
 app.get('*', (_req, res) => {
   res.sendFile(path.join(__dirname, 'app/index.html'));
+});
+
+connectDB(process.env.MONGODB_URL).then(() => {
+  console.log('Database connected');
+  app.listen(PORT, () => {
+    console.log(`Fit2gether is listening at http://localhost:${PORT}`);
+  });
 });
---
+++ src/utils/database.ts
@@ -0,0 +1,25 @@
+import { Collection, MongoClient } from 'mongodb';
+import { Group } from '../types';
+
+let client: MongoClient;
+export const connectDB = async (url: string): Promise<void> => {
+  client = new MongoClient(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  await client.connect();
+  const databasesList = await client.db().admin().listDatabases();
+  console.log(databasesList);
+};
+
+export const disconnectDB = async (): Promise<void> => {
+  await client.close();
+};
+
+export const getCollection = <T>(name: string): Collection<T> => {
+  return client.db().collection<T>(name);
+};
+
+export const getGroupsCollection = (): Collection<Group> => {
+  return getCollection<Group>('groups');
+};

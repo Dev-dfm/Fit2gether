d1f4f7442ef19c8fdde9712741f40580fd56d7af
Dev-dfm <d.floresmedina@gmail.com>
2021-06-23 07:21:54 +0200

    ✨ Add post method for user

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,4 +1,4 @@
-import { Group } from '../../types';
+import { Group, User } from '../../types';
 
 export async function postGroup(group: Group): Promise<Group> {
   const response = await fetch('/api/groups', {
@@ -13,5 +13,21 @@ export async function postGroup(group: Group): Promise<Group> {
     throw errorMessage;
   }
   const result: Group = await response.json();
+  return result;
+}
+
+export async function postUser(user: User): Promise<User> {
+  const response = await fetch('/api/users', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  });
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    throw errorMessage;
+  }
+  const result: User = await response.json();
   return result;
 }

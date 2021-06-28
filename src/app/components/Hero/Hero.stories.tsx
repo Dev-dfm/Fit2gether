8b583e4b311174ee06f69c06f748b09f4f78abe0
Dev-dfm <d.floresmedina@gmail.com>
2021-06-28 15:43:02 +0200

    ♻️ Lift state up from Hero to Main

--- src/app/components/Hero/Hero.stories.tsx
+++ src/app/components/Hero/Hero.stories.tsx
@@ -6,4 +6,6 @@ export default {
   component: Hero,
 };
 
-export const HeroContainer = (): JSX.Element => <Hero />;
+export const HeroContainer = (): JSX.Element => (
+  <Hero search="" setSearch={console.log} />
+);

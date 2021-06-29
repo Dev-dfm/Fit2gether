0d1305ed5de5fe5ba54243de826090c782fac742
Dev-dfm <d.floresmedina@gmail.com>
2021-06-29 15:28:46 +0200

    ✨ Add link between card component and details page

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -5,6 +5,7 @@ import ClockIcon from '../Icons/ClockIcon';
 import DistanceIcon from '../Icons/DistanceIcon';
 import { GroupCard } from '../../../types';
 import { toFormattedDate } from '../../utils/date';
+import { Link } from 'react-router-dom';
 
 export default function Card({
   groupname,
@@ -15,38 +16,40 @@ export default function Card({
 }: GroupCard): JSX.Element {
   const [day, month] = toFormattedDate(date);
   return (
-    <div className={styles.card}>
-      <div className={styles.imageFrame}>
-        <div className={styles.image}></div>
-      </div>
+    <Link to={`/GroupDetails?groupname=${groupname}`}>
+      <div className={styles.card}>
+        <div className={styles.imageFrame}>
+          <div className={styles.image}></div>
+        </div>
 
-      <article className={styles.card__article}>
-        <section className={styles.card__header}>
-          <div className={styles.card__title}>
-            <h1>{groupname}</h1>
-            <p>{location}</p>
-          </div>
-          <div className={styles.icon}>
-            <Boxing />
-          </div>
-        </section>
-        <section className={styles.card__footer}>
-          <div className={styles.card__footerInfo}>
-            <span>{month}</span>
-            <span className={styles.card__footerInfo_day}>{day}</span>
-          </div>
-          <div className={styles.card__footerInfo}>
-            <ClockIcon />
-            <span className={styles.card__footerInfo_timeDist}>{time}</span>
-          </div>
-          <div className={styles.card__footerInfo}>
-            <DistanceIcon />
-            <span className={styles.card__footerInfo_timeDist}>
-              {distance} km
-            </span>
-          </div>
-        </section>
-      </article>
-    </div>
+        <article className={styles.card__article}>
+          <section className={styles.card__header}>
+            <div className={styles.card__title}>
+              <h1>{groupname}</h1>
+              <p>{location}</p>
+            </div>
+            <div className={styles.icon}>
+              <Boxing />
+            </div>
+          </section>
+          <section className={styles.card__footer}>
+            <div className={styles.card__footerInfo}>
+              <span>{month}</span>
+              <span className={styles.card__footerInfo_day}>{day}</span>
+            </div>
+            <div className={styles.card__footerInfo}>
+              <ClockIcon />
+              <span className={styles.card__footerInfo_timeDist}>{time}</span>
+            </div>
+            <div className={styles.card__footerInfo}>
+              <DistanceIcon />
+              <span className={styles.card__footerInfo_timeDist}>
+                {distance} km
+              </span>
+            </div>
+          </section>
+        </article>
+      </div>
+    </Link>
   );
 }

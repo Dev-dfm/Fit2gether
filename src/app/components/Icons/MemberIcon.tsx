3639611079c82413b885052cd8d295e6c5db50e4
Dev-dfm <d.floresmedina@gmail.com>
2021-07-02 23:01:37 +0200

    ♻️ Add className props to icons

--- src/app/components/Icons/MemberIcon.tsx
+++ src/app/components/Icons/MemberIcon.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
-export default function MemberIcon(): JSX.Element {
+type IconProps = {
+  className?: string;
+};
+
+export default function MemberIcon({ className }: IconProps): JSX.Element {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="25" viewBox="0 0 20 20">
+    <svg className={className} width="25" viewBox="0 0 20 20">
       <g transform="translate(-1.995 -2.996)">
         <path
           d="M12,6.6a3.446,3.446,0,1,0,3.439,3.446A3.463,3.463,0,0,0,12,6.6Zm0,2a1.446,1.446,0,1,1-1.446,1.446A1.426,1.426,0,0,1,12,8.6Zm0,6.169A6.512,6.512,0,0,0,5.66,19.828a1,1,0,1,0,1.948.443,4.5,4.5,0,0,1,8.774,0,1,1,0,1,0,1.948-.443A6.5,6.5,0,0,0,12,14.774Z"
